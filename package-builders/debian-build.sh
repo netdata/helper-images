14497e53cf962f762a01a8f5f138f964d31b71cc
James Mills <1290234+prologic@users.noreply.github.com>
2020-01-16 17:47:15 +1000

    Fixed code style via shfmt

--- package-builders/debian-build.sh
+++ package-builders/debian-build.sh
@@ -4,8 +4,8 @@
 # The use of cut and tr here is to handle Ubuntu's multi-word codenames with capital letters.
 CODENAME="$(awk -F"[)(]+" '/VERSION=/ {print $2}' /etc/os-release | cut -f 1 -d ' ' | tr '[:upper:]' '[:lower:]')"
 
-if [ -z "${BUILD_DATE}" ] ; then
-    BUILD_DATE="$(date +%F)"
+if [ -z "${BUILD_DATE}" ]; then
+	BUILD_DATE="$(date +%F)"
 fi
 
 cd /netdata || exit 1
@@ -13,8 +13,8 @@ cd /netdata || exit 1
 cp -a contrib/debian debian || exit 1
 
 # If there's a specific control file for this OS release, use it
-if [ -e "debian/control.${CODENAME}" ] ; then
-    cp "debian/control.${CODENAME}" debian/control || exit 1
+if [ -e "debian/control.${CODENAME}" ]; then
+	cp "debian/control.${CODENAME}" debian/control || exit 1
 fi
 
 # If the changelog was not updated on the host, assume this is a
@@ -23,7 +23,7 @@ sed -i "s/PREVIOUS_PACKAGE_VERSION/${VERSION}/g" debian/changelog
 sed -i "s/PREVIOUS_PACKAGE_DATE/${BUILD_DATE}/g" debian/changelog
 
 # pre/post options are after 1.18.8, is simpler to just check help for their existence than parsing version
-if dpkg-buildpackage --help | grep "\-\-post\-clean" 2> /dev/null > /dev/null; then
+if dpkg-buildpackage --help | grep "\-\-post\-clean" 2>/dev/null >/dev/null; then
 	dpkg-buildpackage --post-clean --pre-clean -b -us -uc || exit 1
 else
 	dpkg-buildpackage -b -us -uc || exit 1
