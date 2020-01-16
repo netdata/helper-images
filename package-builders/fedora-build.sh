ab36553c777535d28b01966a69b56ea42508e88b
James Mills <1290234+prologic@users.noreply.github.com>
2020-01-16 18:33:19 +1000

    Copy/Move *.rpm packages to /netdata/artifacts to support more generalized Build/Install/Test flows via Docker

--- package-builders/fedora-build.sh
+++ package-builders/fedora-build.sh
@@ -19,5 +19,12 @@ sed -i "s/@PACKAGE_VERSION@/${VERSION}/g" /root/rpmbuild/SPECS/netdata.spec || e
 
 rpmbuild -bb --rebuild /root/rpmbuild/SPECS/netdata.spec || exit 1
 
+# Copy the built packages to /netdata/artifacts (which may be bind-mounted)
+# Also ensure /netdata/artifacts exists and create it if it doesn't
 [ -d /netdata/artifacts ] || mkdir -p /netdata/artifacts
-find /root/rpmbuild/RPMS/ -type f -name '*.rpm' -exec cp '{}' /netdata/artifacts \; || exit 1
+find /root/rpmbuild/RPMS/ -type f -name '*.rpm' -exec cp '{}' /netdata/artifacts/ \; || exit 1
+
+# Correct ownership of the artifacts.
+# Without this, the artifacts directory and it's contents end up owned
+# by root instead of the local user on Linux boxes
+chown -R --reference=/netdata /netdata/artifacts
