6f8bb9c0f21608c41af7b1b54fa4515bcce63b32
Austin S. Hemmelgarn <austin@netdata.cloud>
2020-01-17 07:38:53 -0500

    Add package builder images for OpenSUSE. (#34)
    * Add package builder images for OpenSUSE.
    
    * Consistency with master.
    
    * Update default version for consistency with other builders.

---
+++ package-builders/suse-build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cp /netdata/netdata.spec.in /usr/src/packages/SPECS/netdata.spec || exit 1
+
+cp -a /netdata "/usr/src/packages/SOURCES/netdata-${VERSION}" || exit 1
+
+# These next few steps prep the spec file for building with local sources.
+# Without this, we would have to create a tarball of the local sources
+# that the RPM build process would then promptly extract, which is a waste
+# of time.
+#
+# We still use release tarballs for release builds, but that's handled
+# outside of the container.
+sed -i 's/^Source.*$//g' /usr/src/packages/SPECS/netdata.spec || exit 1
+sed -i 's/^%setup.*$/cd %{_topdir}\n rm -rf BUILD\n cp -rf %{_topdir}\/SOURCES\/netdata-%{version} BUILD/g' /usr/src/packages/SPECS/netdata.spec || exit 1
+
+# This updates the version in the spec file appropriately.
+sed -i "s/@PACKAGE_VERSION@/${VERSION}/g" /usr/src/packages/SPECS/netdata.spec || exit 1
+
+rpmbuild -bb --rebuild /usr/src/packages/SPECS/netdata.spec || exit 1
+
+[ -d /netdata/artifacts ] || mkdir -p /netdata/artifacts
+find /usr/src/packages/RPMS/ -type f -name '*.rpm' -exec cp '{}' /netdata \; || exit 1
