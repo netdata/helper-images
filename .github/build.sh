8ae8b31abb969252ece440362574e7fe7225456f
James Mills <1290234+prologic@users.noreply.github.com>
2020-02-04 09:56:30 +1000

    Remove run as .travis/... condition

--- .github/build.sh
+++ .github/build.sh
@@ -9,27 +9,22 @@
 
 set -e
 
-if [ ! -f .travis.yml ]; then
-	echo "Run as ./.travis/$(basename "$0") from top level directory of git repository"
-	exit 1
-fi
-
 echo "Initiating helper image building"
 
 if [ ! -z ${ARCH+x} ]; then
-	# Specified architecture
-	declare -a ARCHITECTURES=(${ARCH})
+  # Specified architecture
+  declare -a ARCHITECTURES=(${ARCH})
 elif [ -z ${DEVEL+x} ]; then
-	# Default architectures
-	declare -a ARCHITECTURES=(i386 armhf aarch64 amd64)
+  # Default architectures
+  declare -a ARCHITECTURES=(i386 armhf aarch64 amd64)
 else
-	# Devel amd64 only
-	declare -a ARCHITECTURES=(amd64)
+  # Devel amd64 only
+  declare -a ARCHITECTURES=(amd64)
 fi
 
 if [ ! -z ${DEVEL+x} ]; then
-	unset DOCKER_PASSWORD
-	unset DOCKER_USERNAME
+  unset DOCKER_PASSWORD
+  unset DOCKER_USERNAME
 fi
 
 # Start paravirtualization
@@ -37,20 +32,20 @@ docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 # Build images using multi-arch Dockerfile.
 for repo in builder base; do
-	for ARCH in "${ARCHITECTURES[@]}"; do
-		BUILD_ARCH="${ARCH}-v3.9"
-		echo "Building docker image ${repo}:${BUILD_ARCH}"
-		eval docker build \
-			--build-arg ARCH="${BUILD_ARCH}" \
-			--tag "netdata/${repo}:${ARCH}" \
-			--file "${repo}/Dockerfile" ./
-	done
+  for ARCH in "${ARCHITECTURES[@]}"; do
+    BUILD_ARCH="${ARCH}-v3.9"
+    echo "Building docker image ${repo}:${BUILD_ARCH}"
+    eval docker build \
+      --build-arg ARCH="${BUILD_ARCH}" \
+      --tag "netdata/${repo}:${ARCH}" \
+      --file "${repo}/Dockerfile" ./
+  done
 done
 
 # There is no reason to continue if we cannot log in to docker hub
 if [ -z ${DOCKER_USERNAME+x} ] || [ -z ${DOCKER_PASSWORD+x} ]; then
-	echo "No docker hub username or password specified. Exiting without pushing images to registry"
-	exit 0
+  echo "No docker hub username or password specified. Exiting without pushing images to registry"
+  exit 0
 fi
 
 # Login to docker hub to allow futher operations
@@ -58,10 +53,10 @@ echo "$DOCKER_PASSWORD" | docker --config /tmp/docker login -u "$DOCKER_USERNAME
 
 # Push images to registry
 for repo in builder base; do
-	for ARCH in "${ARCHITECTURES[@]}"; do
-		echo "Publishing image ${repo}:${ARCH}"
-		docker --config /tmp/docker push "netdata/${repo}:${ARCH}"
-	done
+  for ARCH in "${ARCHITECTURES[@]}"; do
+    echo "Publishing image ${repo}:${ARCH}"
+    docker --config /tmp/docker push "netdata/${repo}:${ARCH}"
+  done
 done
 
 echo "Done!"
