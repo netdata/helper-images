fb4b04c40567fa69d0e75a0e7f7736631da07963
Austin S. Hemmelgarn <austin@netdata.cloud>
2024-05-23 13:04:59 -0400

    Package builder updates. (#289)
    * Fix distro info embedding for CPack DEB packages.
    
    * Add v2 RPM package builders in preparation for CPack RPM support.
    
    * Fix build failures.

--- package-builders/cpack-rpm.sh
+++ package-builders/cpack-rpm.sh
@@ -7,32 +7,23 @@ BUILD_DIR=/build
 DISTRO="$(awk -F'=' '/^ID=/ {print $2}' /etc/os-release)"
 DISTRO_VERSION="$(awk -F'"' '/VERSION_ID=/ {print $2}' /etc/os-release)"
 
+mkdir -p /usr/src
 cp -a /netdata "${SRC_DIR}" || exit 1
 rm -rf "${SRC_DIR}/.git" || exit 1
 
 # Properly mark the installation type.
 cat > "${SRC_DIR}/system/.install-type" <<-EOF
-	INSTALL_TYPE='binpkg-deb'
+	INSTALL_TYPE='binpkg-rpm'
 	PREBUILT_ARCH='$(uname -m)'
 	PREBUILT_DISTRO='${DISTRO} ${DISTRO_VERSION}'
 	EOF
 
-"${SRC_DIR}/packaging/build-package.sh" DEB "${BUILD_DIR}"
+"${SRC_DIR}/packaging/build-package.sh" RPM "${BUILD_DIR}"
 
 [ -d /netdata/artifacts ] || mkdir -p /netdata/artifacts
 
-# Embed distro info in package name.
-# This is required to make the repo actually standards compliant wthout packagecloud's hacks.
-distid="${DISTRO}${DISTRO_VERSION}"
-for pkg in "${BUILD_DIR}"/packages/*.deb "${BUILD_DIR}"/packages/*.ddeb; do
-  extension="${pkg##*.}"
-  pkgname="$(basename "${pkg}" "${extension}")"
-  name="$(echo "${pkgname}" | cut -f 1 -d '_')"
-  version="$(echo "${pkgname}" | cut -f 2 -d '_')"
-  arch="$(echo "${pkgname}" | cut -f 3 -d '_')"
-
-  newname="/netdata/artifacts/${name}_${version}+${distid}_${arch}${extension}"
-  mv "${pkg}" "${newname}" || exit 1
+for pkg in "${BUILD_DIR}"/packages/*.rpm ; do
+    mv "${pkg}" /netdata/artifacts || exit 1
 done
 
 # Correct ownership of the artifacts.
