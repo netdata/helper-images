69a5328f77252177e42abbba1975b3e6bf381288
Austin S. Hemmelgarn <austin@netdata.cloud>
2024-07-08 07:50:46 -0400

    Add legacy image for CentOS.

---
+++ .github/scripts/gen-matrix-legacy.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+from ruamel.yaml import YAML
+
+yaml = YAML(typ='safe')
+entries = []
+
+with open('.github/data/matrices.yaml') as f:
+    data = yaml.load(f)
+
+pkgs = data['legacy']
+meta = data['meta']
+
+entries = []
+
+match sys.argv[1]:
+    case 'build':
+        for distro in pkgs['distros']:
+            entries.append({
+                'os': distro['os'],
+            })
+    case 'pr':
+        for distro in pkgs['distros']:
+            for platform in [x for x in distro['platforms'] if x != meta['native-platform']]:
+                entries.append({
+                    'os': distro['os'],
+                    'platform': platform,
+                })
+    case 'publish':
+        for distro in pkgs['distros']:
+            tags = []
+
+            for registry in meta['registries']:
+                tags.append(f'{registry}{meta["image-prefix"]}{pkgs["image"]}:{distro["os"]}')
+
+            entries.append({
+                'os': distro['os'],
+                'platforms': ','.join(distro['platforms']),
+                'tags': ','.join(tags)
+            })
+    case _:
+        raise ValueError(f'Unrecognized matrix type {sys.argv[1]}')
+
+entries.sort(key=lambda k: k['revision'])
+matrix = json.dumps({'include': entries}, sort_keys=True)
+print(matrix)
